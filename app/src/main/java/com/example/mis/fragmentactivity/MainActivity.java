8d2df367aa38e7534241455bb177e8b0cc7fdd19
pokar-ajay <pokarajay1@gmail.com>
2017-12-29 17:23:00 +0530

    Third

--- app/src/main/java/com/example/mis/fragmentactivity/MainActivity.java
+++ app/src/main/java/com/example/mis/fragmentactivity/MainActivity.java
@@ -24,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
                 f.replace(R.id.change,new BlankFragment());
                 f.commit();
 
+
+
+                
             }
         });
     }
